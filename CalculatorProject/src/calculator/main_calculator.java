24050005494dcf8f54a56c9dab27007f29c09363
Nilesh9984 <singhnilesh199@gmail.com>
2020-05-19 21:51:06 +0530

    add calculator project

---
+++ CalculatorProject/src/calculator/main_calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class main_calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		calculator cals = new calculator();
+		
+	}
+
+}
